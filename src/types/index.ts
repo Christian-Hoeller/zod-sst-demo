3fb59251d339b14ad536fc2d77cae8eb148d8934
Sev <sevm@pm.me>
2024-03-06 19:10:22 +0100

    build: updated tsconfig.json

--- src/types/index.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-import { EventSchema, SportSchema, UserSchema } from "../schemas";
-
-export type Event = z.infer<typeof EventSchema>;
-export type Sport = z.infer<typeof SportSchema>;
-export type User = z.infer<typeof UserSchema>;
