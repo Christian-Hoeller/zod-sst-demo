85e90dd011d7621d1f2a67c5689f58c1e42d648e
Sev <sevm@pm.me>
2024-03-06 18:38:48 +0100

    chore: moved openapi generator to utils

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,4 @@
 import { extendZodWithOpenApi } from "@asteasolutions/zod-to-openapi";
 import { z } from "zod";
 
-// extend zod with openapi. This will add the openapi method to all zod types
-// it should also only be called once in the application
 extendZodWithOpenApi(z);
-z.string().openapi({ description: "Some string" });
-
-import { OpenApiGeneratorV3 } from "@asteasolutions/zod-to-openapi";
-import yaml from "yaml";
-import fs from "fs";
-import { SportSchema, UserSchema } from "./schemas";
-
-const openApiConfig = {
-  openapi: "3.0.3",
-  info: {
-    version: "1.0.0",
-    title: "Demo API",
-    description: "Demo API for OpenAPI documentation",
-  },
-  servers: [{ url: "v1" }],
-};
-
-const generateOpenApiDocumentation = () => {
-  const generator = new OpenApiGeneratorV3([
-    // @ts-ignore
-    SportSchema,
-    // @ts-ignore
-    UserSchema,
-  ]);
-  const { components } = generator.generateComponents();
-
-  return generator.generateDocument({
-    ...openApiConfig,
-    ...(components && { components }),
-  });
-};
-
-function writeDocumentationToYamlFile() {
-  const docs = generateOpenApiDocumentation();
-
-  const fileContent = yaml.stringify(docs);
-
-  fs.writeFileSync(`./docs/openApi.yml`, fileContent, {
-    encoding: "utf-8",
-  });
-}
-
-writeDocumentationToYamlFile();
